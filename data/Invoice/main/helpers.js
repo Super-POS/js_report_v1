9927f3d5439e898f6d48616b46dbc4de95a72e7c
bunkheangheng <bunkheangheng99@gmail.com>
2026-05-04 00:18:19 +0700

    fixed

--- data/Invoice/main/helpers.js
+++ data/Invoice/main/helpers.js
@@ -1,12 +1,17 @@
 var n=0;
+function fmtUsd(x){
+    var n = Number(x);
+    if (!isFinite(n)) return '$0.00';
+    return '$' + n.toLocaleString('en-US', { minimumFractionDigits: 2, maximumFractionDigits: 2 });
+}
 function price(unit_price){
-    return Math.round(unit_price).toLocaleString();
+    return fmtUsd(unit_price);
 }
 function amount(unit_price,qty){
-    return Math.round(unit_price * qty).toLocaleString();
+    return fmtUsd(unit_price * qty);
 }
 function total_amount(total){
-    return Math.round(total).toLocaleString();
+    return fmtUsd(total);
 }
 function increas() {
     n++;
